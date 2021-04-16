973c9a473ff6bd6e9e3f50cacdea5fc039244f83
Nimesh Nelanga <Nimesh.N@CreativeSoftware.com>
2021-04-17 03:06:13 +0530

    Added remaining models and migrations

---
+++ database/migrations/2021_04_16_212033_create_centers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCentersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('centers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->double('latitude')->nullable();
+            $table->double('longitude')->nullable();
+            $table->string('contactNumber');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('centers');
+    }
+}
---
+++ database/migrations/2021_04_16_212627_create_clinics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClinicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clinics', function (Blueprint $table) {
+            $table->id();
+            $table->string('dayOfWeek');
+            $table->timeTz('time');
+            $table->unsignedBigInteger('center_id');
+            $table->string('title');
+            $table->timestamps();
+
+            $table->index('center_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clinics');
+    }
+}
